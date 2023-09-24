298e6792acc4a92747f0446ed232356c7e8c9835
Nikita <nikita.ushackow.02@gmail.com>
2023-09-24 22:08:55 +0200

    lab; mini change in structure

--- c#/Derivative/Derivative/Program.cs
+++ c#/Derivative/Derivative/Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main()
     {
-        // Создаем объекты родительского и унаследованного классов
+        // Пример функции x^2
+        Func<double, double> function = x => x * x;
+
+        // Используется для приближенного вычисления производных в определенной точке функции
+        double h = 0.01; 
+        double x = 4.0;
+
         DerivativeCalculator derivativeCalculator = new DerivativeCalculator();
-
-        // Пример использования методов для вычисления производных
-        Func<double, double> function = x => x * x; // Пример функции x^2
-
-        double x = 2.0;
-        double h = 0.01;
 
         double rightDerivative = derivativeCalculator.CalculateRightDerivative(function, x, h);
         double leftDerivative = derivativeCalculator.CalculateLeftDerivative(function, x, h);
@@ -22,3 +22,12 @@ class Program
         Console.WriteLine($"Central Derivative: {centralDerivative}");
     }
 }
+
+/*Симметрия функции: Если функция, для которой вы вычисляете производные, симметрична относительно 
+точки x, то правая и левая производные могут быть близкими по значению. Например, для функции 
+f(x) = x ^ 2, производные в точке x и -x будут иметь одинаковые значения.*/
+
+/*Малый шаг h: Если вы выбрали очень маленькое значение h, то разница между правой и левой производными 
+становится невеликой, так как h представляет собой малое приращение аргумента функции, и разница 
+между x + h и x - h становится незначительной. При таком маленьком h и правая, и левая производные 
+приближают мгновенную скорость изменения функции в точке x с высокой точностью.*/
--- c#/Ratio/Ratio/Program.cs
+++ c#/Ratio/Ratio/Program.cs
@@ -8,56 +8,7 @@
 
             try
             {
-                Console.WriteLine("Введите первую дробь (в формате 'числитель/знаменатель'):");
-                string input1 = Console.ReadLine();
-                string[] parts1 = input1.Split('/');
-                int numerator1 = int.Parse(parts1[0]);
-                int denominator1 = int.Parse(parts1[1]);
-                RatioCalc frac1 = new RatioCalc(numerator1, denominator1);
-
-                Console.WriteLine("Введите вторую дробь (в формате 'числитель/знаменатель'):");
-                string input2 = Console.ReadLine();
-                string[] parts2 = input2.Split('/');
-                int numerator2 = int.Parse(parts2[0]);
-                int denominator2 = int.Parse(parts2[1]);
-                RatioCalc frac2 = new RatioCalc(numerator2, denominator2);
-
-                Console.WriteLine("Выберите операцию:");
-                Console.WriteLine("1. Сложение");
-                Console.WriteLine("2. Вычитание");
-                Console.WriteLine("3. Умножение");
-                Console.WriteLine("4. Деление");
-                Console.WriteLine("5. Нахождение обратной дроби");
-                Console.WriteLine("6. Преобразование в десятичное представление");
-
-                int choice = int.Parse(Console.ReadLine());
-
-                switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine($"Результат: {frac1 + frac2}");
-                        break;
-                    case 2:
-                        Console.WriteLine($"Результат: {frac1 - frac2}");
-                        break;
-                    case 3:
-                        Console.WriteLine($"Результат: {frac1 * frac2}");
-                        break;
-                    case 4:
-                        Console.WriteLine($"Результат: {frac1 / frac2}");
-                        break;
-                    case 5:
-                        Console.WriteLine($"Обратная дробь для первой дроби: {frac1.GetReciprocal()}");
-                        Console.WriteLine($"Обратная дробь для второй дроби: {frac2.GetReciprocal()}");
-                        break;
-                    case 6:
-                        Console.WriteLine($"Десятичное представление первой дроби: {frac1.ToDecimal()}");
-                        Console.WriteLine($"Десятичное представление второй дроби: {frac2.ToDecimal()}");
-                        break;
-                    default:
-                        Console.WriteLine("Неверный выбор операции.");
-                        break;
-                }
+                ActionsWithRatios();
             }
             catch (FormatException)
             {
@@ -75,6 +26,63 @@
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        static void ActionsWithRatios()
+        {
+            Console.WriteLine("Введите первую дробь (в формате 'числитель/знаменатель'):");
+            RatioCalc frac1 = GetEnteredRatio();
+
+            Console.WriteLine("Введите вторую дробь (в формате 'числитель/знаменатель'):");
+            RatioCalc frac2 = GetEnteredRatio();
+
+            Console.WriteLine("Выберите операцию:");
+            Console.WriteLine("1. Сложение");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+            Console.WriteLine("5. Нахождение обратной дроби");
+            Console.WriteLine("6. Преобразование в десятичное представление");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"Результат: {frac1 + frac2}");
+                    break;
+                case 2:
+                    Console.WriteLine($"Результат: {frac1 - frac2}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Результат: {frac1 * frac2}");
+                    break;
+                case 4:
+                    Console.WriteLine($"Результат: {frac1 / frac2}");
+                    break;
+                case 5:
+                    Console.WriteLine($"Обратная дробь для первой дроби: {frac1.GetReciprocal()}");
+                    Console.WriteLine($"Обратная дробь для второй дроби: {frac2.GetReciprocal()}");
+                    break;
+                case 6:
+                    Console.WriteLine($"Десятичное представление первой дроби: {frac1.ToDecimal()}");
+                    Console.WriteLine($"Десятичное представление второй дроби: {frac2.ToDecimal()}");
+                    break;
+                default:
+                    Console.WriteLine("Выбран неверный тип (разрешён только от 1 до 7).");
+                    break;
+            }
+        }
+
+        static RatioCalc GetEnteredRatio()
+        {
+            string input2 = Console.ReadLine();
+            string[] parts2 = input2.Split('/');
+
+            int numerator2 = int.Parse(parts2[0]);
+            int denominator2 = int.Parse(parts2[1]);
+
+            return new RatioCalc(numerator2, denominator2);
         }
     }
 }
