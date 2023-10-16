c1b5aff0cf72bafd4767dcd713c86b315314a47f
Nikita <nikita.ushackow.02@gmail.com>
2023-10-16 21:47:48 +0200

    lab: write class - array3d and test for this class

---
+++ c#/ActionsWithDimensionalArray/ActionsWithDimensionalArray/Array3d.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace ActionsWithDimensionalArray
+{
+    public class Array3d<T>
+    {
+        private T[] data;
+        private int dim0, dim1, dim2;
+
+        public Array3d(int dim0, int dim1, int dim2)
+        {
+            if (dim0 <= 0 || dim1 <= 0 || dim2 <= 0)
+                throw new ArgumentException("Dimensions must be greater than zero.");
+
+            this.dim0 = dim0;
+            this.dim1 = dim1;
+            this.dim2 = dim2;
+            this.data = new T[dim0 * dim1 * dim2];
+        }
+
+        /// <summary>
+        /// Индексатор для получения и присваивания значения
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        public T this[int i, int j, int k]
+        {
+            get
+            {
+                if (i < 0 || i >= dim0 || j < 0 || j >= dim1 || k < 0 || k >= dim2)
+                    throw new IndexOutOfRangeException("Index is out of range.");
+
+                return data[i * (dim1 * dim2) + j * dim2 + k];
+            }
+            set
+            {
+                if (i < 0 || i >= dim0 || j < 0 || j >= dim1 || k < 0 || k >= dim2)
+                    throw new IndexOutOfRangeException("Index is out of range.");
+
+                data[i * (dim1 * dim2) + j * dim2 + k] = value;
+            }
+        }
+
+        public T[] GetValues0(int i)
+        {
+            if (i < 0 || i >= dim0)
+                throw new IndexOutOfRangeException("Index is out of range.");
+
+            T[] result = new T[dim1 * dim2];
+            Array.Copy(data, i * (dim1 * dim2), result, 0, dim1 * dim2);
+            return result;
+        }
+
+        public T[] GetValues1(int j)
+        {
+            if (j < 0 || j >= dim1)
+                throw new IndexOutOfRangeException("Index is out of range.");
+
+            T[] result = new T[dim0 * dim2];
+            for (int i = 0; i < dim0; i++)
+                for (int k = 0; k < dim2; k++)
+                    result[i * dim2 + k] = data[i * (dim1 * dim2) + j * dim2 + k];
+
+            return result;
+        }
+
+        public T[] GetValues2(int k)
+        {
+            if (k < 0 || k >= dim2)
+                throw new IndexOutOfRangeException("Index is out of range.");
+
+            T[] result = new T[dim0 * dim1];
+            for (int i = 0; i < dim0; i++)
+                for (int j = 0; j < dim1; j++)
+                    result[i * dim1 + j] = data[i * (dim1 * dim2) + j * dim2 + k];
+
+            return result;
+        }
+
+        public void SetValue1(int j, T[] values)
+        {
+            if (j < 0 || j >= dim1)
+                throw new IndexOutOfRangeException("Index is out of range.");
+
+            if (values.Length != dim0 * dim2)
+                throw new ArgumentException("Input array length does not match the dimensions.");
+
+            for (int i = 0; i < dim0; i++)
+                for (int k = 0; k < dim2; k++)
+                    data[i * (dim1 * dim2) + j * dim2 + k] = values[i * dim2 + k];
+        }
+
+        public void SetValue2(int k, T[] values)
+        {
+            if (k < 0 || k >= dim2)
+                throw new IndexOutOfRangeException("Index is out of range.");
+
+            if (values.Length != dim0 * dim1)
+                throw new ArgumentException("Input array length does not match the dimensions.");
+
+            for (int i = 0; i < dim0; i++)
+                for (int j = 0; j < dim1; j++)
+                    data[i * (dim1 * dim2) + j * dim2 + k] = values[i * dim1 + j];
+        }
+
+        public static Array3d<T> CreateUniform(int dim0, int dim1, int dim2, T value)
+        {
+            Array3d<T> array3d = new Array3d<T>(dim0, dim1, dim2);
+            for (int i = 0; i < dim0; i++)
+                for (int j = 0; j < dim1; j++)
+                    for (int k = 0; k < dim2; k++)
+                        array3d[i, j, k] = value;
+
+            return array3d;
+        }
+    }
+}
--- c#/ActionsWithDimensionalArray/ActionsWithDimensionalArray/Program.cs
+++ c#/ActionsWithDimensionalArray/ActionsWithDimensionalArray/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ActionsWithDimensionalArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создаем трехмерный массив
+            Array3d<int> array = new Array3d<int>(3, 3, 3);
+
+            // Заполняем массив значениями
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        array[i, j, k] = i * 100 + j * 10 + k;
+                    }
+                }
+            }
+
+            // Получаем и выводим значения
+            Console.WriteLine("Array values:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Console.WriteLine($"array[{i},{j},{k}] = {array[i, j, k]}");
+                    }
+                }
+            }
+
+            // Получаем и выводим значения по измерению 0 (фиксированный i)
+            int[] values0 = array.GetValues0(1);
+            Console.WriteLine("Values along dimension 0 (i=1): " + string.Join(", ", values0));
+
+            // Получаем и выводим значения по измерению 1 (фиксированный j)
+            int[] values1 = array.GetValues1(2);
+            Console.WriteLine("Values along dimension 1 (j=2): " + string.Join(", ", values1));
+
+            // Получаем и выводим значения по измерению 2 (фиксированный k)
+            int[] values2 = array.GetValues2(0);
+            Console.WriteLine("Values along dimension 2 (k=0): " + string.Join(", ", values2));
+        }
+    }
+}
