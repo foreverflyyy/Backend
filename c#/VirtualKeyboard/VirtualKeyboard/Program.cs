48253de0bf86993807d6bf7cc76250e8d60ef72d
Nikita <nikita.ushackow.02@gmail.com>
2023-10-26 00:06:58 +0200

    lab: start working over lab number 4

---
+++ c#/VirtualKeyboard/VirtualKeyboard/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/*1) Создать виртуальную клавиатуру с переназначаемыми действиями для клавиш и комбинаций клавиш,
+    с возможностью отката действий назад.
+2) Продемонстрировать работу клавиатуры сделал Workflow из нажатий различных комбинаций клавиш
+    и откатов назад. Симулировать демонстрацию нажатий клавиш путем вывода значения в консоль и 
+    задержкой между нажатиями
+3) Продемонстрировать переназначение клавиши и комбинации клавиш с перезапуком Workflow*/
+
+namespace VirtualKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VirtualKeyboard virtualKeyboard = new VirtualKeyboard();
+
+            virtualKeyboard.AddKeyAction(ConsoleKey.A, () => Console.WriteLine("Key A Pressed"));
+            virtualKeyboard.AddKeyAction(ConsoleKey.B, () => Console.WriteLine("Key B Pressed"));
+
+            virtualKeyboard.DisplayKeyActions();
+
+            virtualKeyboard.AddActionToWorkflow(() => virtualKeyboard.ExecuteKeyAction(ConsoleKey.A));
+            virtualKeyboard.AddActionToWorkflow(() => virtualKeyboard.ExecuteKeyAction(ConsoleKey.B));
+            virtualKeyboard.AddActionToWorkflow(() => Console.WriteLine("Custom Action"));
+
+            Console.WriteLine("Executing Workflow:");
+            virtualKeyboard.ExecuteWorkflow();
+
+            Console.WriteLine("Undo Last Action:");
+            virtualKeyboard.UndoLastAction();
+            virtualKeyboard.ExecuteWorkflow();
+        }
+    }
+}
---
+++ c#/VirtualKeyboard/VirtualKeyboard/VirtualKeyboard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace VirtualKeyboard
+{
+    public class VirtualKeyboard
+    {
+        private Dictionary<ConsoleKey, Action> keyActions = new Dictionary<ConsoleKey, Action>();
+        private List<Action> workflow = new List<Action>();
+
+        public void AddKeyAction(ConsoleKey key, Action action)
+        {
+            keyActions[key] = action;
+        }
+
+        public void ExecuteWorkflow()
+        {
+            foreach (var action in workflow)
+            {
+                action.Invoke();
+                Thread.Sleep(1000); // Задержка между действиями (1 секунда)
+            }
+        }
+
+        public void AddActionToWorkflow(Action action)
+        {
+            workflow.Add(action);
+        }
+
+        public void UndoLastAction()
+        {
+            if (workflow.Count > 0)
+                workflow.RemoveAt(workflow.Count - 1);
+        }
+
+        public void DisplayKeyActions()
+        {
+            Console.WriteLine("Key Actions:");
+            foreach (var keyAction in keyActions)
+            {
+                Console.WriteLine($"{keyAction.Key}: {keyAction.Value.Method.Name}");
+            }
+        }
+    }
+}
