a3ead6f0fe4ae6bd52fa3e494190ad045517812e
Nikita <nikita.ushackow.02@gmail.com>
2023-12-04 18:03:18 +0200

    lab: end labs about authorization

---
+++ typescript/authorization/WorkWithFile.ts
@@ -0,0 +1,31 @@
+import {appendFile, readFileSync} from "fs";
+import {User} from "./User";
+
+export class WorkWithFile {
+    public static readCSVSync = (filePath: string) => {
+        try {
+            const result: User[] = [];
+            const fileContent = readFileSync(filePath, 'utf-8');
+            const rows = fileContent.split('\n');
+
+            for (const row of rows) {
+                const parts = row.split(",");
+                result.push(new User(parts[0], parts[1]));
+            }
+
+            return result;
+        } catch (error) {
+            console.error('Ошибка чтения файла:', error);
+            return [];
+        }
+    }
+
+    public static writeToFile = async (pathToFile: string, data: string) => {
+        appendFile(pathToFile, data, 'utf8', function (err: any) {
+            if (err)
+                console.log("Ошибка.");
+            else
+                console.log("Успешная запись");
+        });
+    }
+}
---
+++ typescript/authorization/index.ts
@@ -0,0 +1,74 @@
+/*
+    Авторизация и регистрация пользователя, данные о пользователях хранит и
+    записывает в csv файл, с возможностью запоминания авторизованного пользователя
+*/
+import {WorkWithFile} from "./WorkWithFile";
+import {User} from "./User";
+
+class Authenticator {
+    private users: User[] = [];
+    private loggedInUser: User | null = null;
+    private readonly csvFilePath: string;
+
+    constructor(csvFilePath: string) {
+        this.csvFilePath = csvFilePath;
+        this.loadUsers();
+    }
+
+    loadUsers() {
+        try {
+            this.users = WorkWithFile.readCSVSync(this.csvFilePath);
+        } catch (error) {
+            this.users = [];
+        }
+    }
+
+    async register (login: string, password: string) {
+        const existingUser = this.users.find((user) => user.login === login);
+        if (existingUser) {
+            console.log('Пользователь с таким логином уже существует.');
+        } else {
+            const newUser = new User(login, password);
+            this.users.push(newUser);
+            await WorkWithFile.writeToFile(this.csvFilePath, `\n${login},${password}`);
+            console.log('Регистрация пользователя успешна.');
+        }
+    }
+
+    login(login: string, password: string): void {
+        const user = this.users.find((u) => u.login === login && u.password === password);
+
+        if (user) {
+            this.loggedInUser = user;
+            console.log('Успешный логин.');
+        } else {
+            console.log('Неверный логин или пароль.');
+        }
+    }
+
+    logout(): void {
+        this.loggedInUser = null;
+        console.log('Logout.');
+    }
+
+    isLoggedIn(): boolean {
+        return this.loggedInUser !== null;
+    }
+
+    getCurrentUser(): User | null {
+        return this.loggedInUser;
+    }
+}
+
+const authenticator = new Authenticator('data.csv');
+
+authenticator.register('john_doe', 'secure_password');
+authenticator.login('john_doe', 'secure_password');
+
+if (authenticator.isLoggedIn()) {
+    const currentUser = authenticator.getCurrentUser();
+    console.log(`Текущий пользователь: ${currentUser?.login}`);
+} else
+    console.log('Пользователь не залогинен.');
+
+authenticator.logout();
